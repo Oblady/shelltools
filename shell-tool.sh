2c390974502d7d7a204b7f0623313a793f0c6ee0
Beleneglorion <thibault.sebastien@hinatachii.com>
2012-12-19 18:02:51 +0100

    correction commit precedent

--- shell-tool.sh
+++ shell-tool.sh
@@ -4,7 +4,7 @@
 #################################################################
 # Version: 0.1
 # Date: 01/12/2012
-# Author:  Sébastient THIBAULT <sebastien@oblady.fr>
+# Author:  Sébastien THIBAULT <sebastien@oblady.fr>
 #
 # Notes: 
 #
@@ -21,7 +21,7 @@ source ./util.lib.sh
 # de la fonction avant insertion							#
 #############################################################
 function install_cool_bash()  {
-	if [ ! -f ./bash_color.txt ]
+	if [ ! -f ./resources/bash/bash_color.txt ]
 	then
 	  print_warning "bash_color.txt is missing"
 	  return 0;
@@ -31,37 +31,142 @@ function install_cool_bash()  {
 	if [ $result -eq 0 ]
 	then
 	  print_info "Installing bash coloration"
-	  cat ./bash_color.txt >> /etc/bash.bashrc
+	  cat ./resources/bash/bash_color.txt >> /etc/bash.bashrc
     else
 	   print_info "bash coloration already installed" 
 	fi 
 	pause;
 }
 
+#############################################################
+# Installation  de most un pager avec la coloration			#	
+#############################################################
+function install_htop()  {
+	print_info "installing htop"
+    check_and_install htop htop
+    pause
+}
+#############################################################
+# Installation  de most un pager avec la coloration			#	
+#############################################################
+function install_most()  {
+	print_info "installing Most"
+    check_and_install most most
+    print_info "Configuring Most"
+    update-alternatives --set pager /usr/bin/most
+    pause
+}
+#############################################################
+# Installation de nmap										#	
+#############################################################
+function install_nmap()  {
+	print_info "installing Nmap"
+    check_and_install nmap nmap
+    pause
+}
+
+#############################################################
+# Installation de rkhunter									#	
+#############################################################
+function install_rkhunter()  {
+	print_info "installing rkhunter"
+    check_and_install rkhunter rkhunter
+    pause
+}
+
+#############################################################
+# Installation de fail2ban										#	
+#############################################################
+function install_fail2ban()  {
+	print_info "installing fail2ban"
+    check_and_install fail2ban fail2ban
+    pause
+}
+#############################################################
+# Installation de chkrootkit										#	
+#############################################################
+function install_chkrootkit()  {
+	print_info "installing chkrootkit"
+    check_and_install chkrootkit chkrootkit
+    pause
+}
+
+
+#############################################################
+# Installation de nmap										#	
+#############################################################
+function install_logwatch()  {
+	print_info "installing Logwatch"
+    check_and_install logwatch logwatch
+    #ajout des fichier de footer et header html si manquant
+    if [ ! -d /usr/share/logwatch/default.conf/html/ ]
+	then 
+	   mkdir -p /usr/share/logwatch/default.conf/html/
+    fi
+    if [ ! -f /usr/share/logwatch/default.conf/html/footer.html ]
+	then
+	   cp ./resources/logwatch/footer.html /usr/share/logwatch/default.conf/html/
+    fi
+	if [ ! -f /usr/share/logwatch/default.conf/html/header.html ]
+	then
+	   cp ./resources/logwatch/header.html /usr/share/logwatch/default.conf/html/
+    fi
+    #suppression du cron automatique de logwatch qui envoi un mail interne a personne
+    cat /etc/cron.daily/00logwatch |grep -v "\-\-output mail"|grep -v "#execute" > /tmp/newcronlogwatch
+    cat  /tmp/newcronlogwatch > /etc/cron.daily/00logwatch
+    rm /tmp/newcronlogwatch 
+    #recherche d'un email deja existant
+    CURRENT=$(egrep -v "^#" /etc/cron.daily/00logwatch|grep "\-\-mailto"|cut -d" " -f 3) 
+    echo "$CURRENT"
+    pause
+    # ask for a true email to send logwatch
+    EMAIL=$(whiptail --inputbox "Saisir l'email de l'administrateur?" --title "Logwatch admin email" 8 78 "${CURRENT}"  3>&1 1>&2 2>&3)
+	exitstatus=$?
+	if [ $exitstatus = 0 -a -n "$EMAIL" ]
+	then
+	   #ssuppresion de l'email courant pour le remplacer par celui saisi	
+	   cat /etc/cron.daily/00logwatch |grep -v "\-\-mailto ${CURRENT}"> /tmp/newcronlogwatch
+	   cat /tmp/newcronlogwatch > /etc/cron.daily/00logwatch
+	   rm  /tmp/newcronlogwatch
+	   echo "/usr/sbin/logwatch --mailto ${EMAIL} --format html --encode base64" >> /etc/cron.daily/00logwatch
+	fi
+    pause
+}
+
+#############################################################
+# Installation  some security apps							#	
+#############################################################
+function install_security()  {
+	print_info "installing Security Packages"
+	disable_pause
+    install_nmap
+    install_logwatch
+    install_fail2ban
+    install_rkhunter
+    install_chkrootkit
+    enable_pause
+}
+
 ######################################
 # Start Of Programme	     		 #	
 ######################################
 
-
-check_sanity;
+check_sanity
 while : # Loop forever
 do
 clear
-printf "\e[01;31mInstall Tool\e[00m\n";	
-cat << !
- 
-R U N M E N U
- 
-1. Install Cool bash
-q. Quit
- 
-!
-echo -n " Your choice? : "
-read choice
- 
-case $choice in
-1) install_cool_bash ;;
-q) exit ;;
-*) print_warn "$choice n'est pas un choix valide"; sleep 2 ;;
+reset_menu
+set_menu_title "Shell Tools"
+set_menu_query "Please select a tool"
+set_menu_quit "b" "Back" 
+add_menu "Install Cool bash colors" "install_cool_bash"
+add_menu "Install most" "install_most"
+add_menu "Install htop" "install_htop"
+add_menu "Install security packages" "install_security"
+show_menu
+
+case $result in
+b) exit ;;
+*) ${M_ACTIONS[result]};;
 esac
 done
